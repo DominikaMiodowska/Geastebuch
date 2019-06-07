2dbd6f393553bee5cb5157219a409d68b1689dc8
Dominika Miodowska <dm@Dominikas-MacBook-Pro.fritz.box>
2019-06-07 21:13:14 +0200

    x1

---
+++ app.js
@@ -0,0 +1,55 @@
+"use strict"
+
+let fs = require("fs");
+let express = require("express");
+let bodyParser = require("body-parser");
+let GuestbookEntry = require("./src/GuestbookEntry")
+
+
+fs.readFile("./data.json", "utf-8", (err, data) => {    // przeczytaj file
+    if (err) throw err;
+    let d = JSON.parse(data);
+
+    let entries = [];
+    for(let entry of d){
+        entries.push(new GuestbookEntry(entry.title, entry.content));
+    }
+
+    // Starte den Server
+    let app = express();
+    app.set("view engine", "ejs");
+    app.set("views", "./views");
+
+    app.use(bodyParser.urlencoded({extended: true}));       // przesyla dane
+    app.use(express.static("./public"));
+
+
+    app.get("/", (req, res) =>{
+        res.render("index",{
+            entries:  entries
+        });
+    });
+
+    app.post("/guestbook/new", (req, res) => {
+        let content = req.body.content;
+        let title = req.body.title;
+
+        let newEntry = new GuestbookEntry(title, content);
+        entries.push(newEntry);             // *** entris is ein Array   ***
+
+        //zapisz dane w data.json
+        fs.writeFile("./data.json", JSON.stringify(entries), function(err, result) {
+            if(err) console.log('error', err);
+          });       // przeksztalc entries Array na string
+
+
+        res.redirect("/"); // po dodatniu wpisu wroc do strony glownej
+
+    });
+
+
+    app.listen(5000, () => {
+        console.log("App wurde gestartet auf localhost:5000");
+    })
+
+    })
